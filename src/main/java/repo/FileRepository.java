81e3ddd2b383741a4c591d13fee165095bbb359c
evamariat <eva.maria.truve@iCloud.com>
2026-04-05 12:02:49 +0300

    Delete src directory

--- src/main/java/repo/FileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repo;
-
-import com.example.model.UploadedFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<UploadedFile, Long> {
-    // you can add custom methods here later if needed
-}
